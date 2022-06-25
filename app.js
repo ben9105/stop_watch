5298b46902e8a00321e33164aedf5c4545d7a735
ben9105 <benjaminshew@gmail.com>
2022-06-25 12:52:13 -0700

    html and css updated

---
+++ app.js
@@ -0,0 +1,8 @@
+const start = document.getElementById('start-button')
+const stop = document.getElementById('stop-button')
+const reset = document.getElementById('reset-button')
+
+
+start.onclick = function startTimer() {
+    console.log('test');
+}
